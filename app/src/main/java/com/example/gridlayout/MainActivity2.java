2a029435b49605ba29c5c88ce32ee0b53fd0380a
MarcoLaureano <mlaurean19@jaumebalmes.net>
2022-11-17 16:14:40 +0100

    Flecha Marco

--- app/src/main/java/com/example/gridlayout/MainActivity2.java
+++ app/src/main/java/com/example/gridlayout/MainActivity2.java
@@ -5,22 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
         ImageButton btn = (ImageButton) findViewById(R.id.flecha);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, MainActivity2.class));
+                startActivity(new Intent(MainActivity2.this, MainActivity3.class));
             }
         });
     }
